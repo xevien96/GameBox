98cb430e58c3b92ab90fa96d6074ef0b6fba5b0e
Albert <AlbertRenz@web.de>
2017-05-23 13:12:35 +0200

    hallo

--- src/DesktopFrame.java
+++ src/DesktopFrame.java
@@ -1,5 +1,34 @@
+import javax.swing.*;
+
 /**
- * Created by albertrenz on 23.05.17.
+ * @author Johann Helbig, Albert Renz, Marc Brandt
  */
-public class DesktopFrame {
+
+public class DesktopFrame extends JApplet {
+    private JDesktopPane desk;
+
+    public DesktopFrame(){
+        desk = new JDesktopPane();
+        desk.setDesktopManager(new DefaultDesktopManager());
+        setContentPane(desk);
+    }
+
+    public void addChild(JInternalFrame child, int x, int y){
+        child.setLocation(x, y);
+        child.setSize(300,300);
+        child.setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
+        desk.add(child);
+        child.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel( UIManager.getCrossPlatformLookAndFeelClassName() );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        DesktopFrame desktop = new DesktopFrame();
+        desktop.addChild(new ChildFrame(desktop),10,10);
+        Konsole.run(desktop,800,600);
+    }
 }
