6232ded9fbdf5daa576ddf885ad94094d9f4fbc3
Albert <AlbertRenz@web.de>
2017-05-23 13:11:43 +0200

    hallo

---
+++ src/DesktopFrame.java
@@ -0,0 +1,5 @@
+/**
+ * Created by albertrenz on 23.05.17.
+ */
+public class DesktopFrame {
+}
---
+++ src/Konsole.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class Konsole {
+    public static String title(Object o) {
+        String t = o.getClass().toString();
+        if(t.indexOf("class") != -1) t = t.substring(6);
+        System.out.println ("Konsole: running "+t);
+        return t;
+    }
+    public static void setupClosing(JFrame frame) {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void run(JFrame frame, int width, int height) {
+        setupClosing(frame);
+        frame.setSize(width, height);
+        frame.setVisible(true);
+    }
+    public static void run(JApplet applet, int width, int height) {
+        JFrame frame = new JFrame(title(applet));
+        setupClosing(frame);
+        frame.getContentPane().add(applet);
+        frame.setSize(width, height);
+        applet.init();
+        applet.start();
+        frame.setVisible(true);
+    }
+    public static void run(JPanel panel, int width, int height) {
+        JFrame frame = new JFrame(title(panel));
+        setupClosing(frame);
+        frame.getContentPane().add(panel);
+        frame.setSize(width, height);
+        frame.setVisible(true);
+    }
+}
