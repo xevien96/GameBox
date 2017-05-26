5e35a2349a4eb6d9306e9cdc9f97f50097746cea
Xevien <xevien96@gmail.com>
2017-05-26 18:42:33 +0200

    Sokoban Anfang, schauen wie man die Level aus der txt bekommt

---
+++ src/sokoban/Level.java
@@ -0,0 +1,10 @@
+package sokoban;
+
+public class Level {
+    private int levelNr;
+
+
+    public Level(int levelNr, String levelName, String level){
+
+    }
+}
--- src/sokoban/LevelMaker.java
+++ src/sokoban/LevelMaker.java
@@ -1,9 +1,8 @@
 package sokoban;
 
 import javax.swing.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Scanner;
 
 public class LevelMaker {
 
@@ -19,9 +18,27 @@ public class LevelMaker {
     public static void makeLevelsFromFile(File levelFile){
         try {
             FileReader input = new FileReader(levelFile);
+            BufferedReader reader = new BufferedReader(input);
+            int levelNr;
+            String levelName = "";
+            String level = "";
+            String temp;
+            while((temp = reader.readLine()) != null){
+                if(temp.startsWith("Level")){
+                    Scanner levelNrScanner = new Scanner(temp);
+                    levelNr = levelNrScanner.nextInt();
+                    temp = reader.readLine();
+                    levelName = temp;
+                    while(!(temp = reader.readLine()).equals("")){
+                        level += temp+"\n";
+                    }
 
+                }
+            }
         } catch (FileNotFoundException e){
             System.out.println("File not found");
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
