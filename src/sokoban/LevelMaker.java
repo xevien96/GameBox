412885189ad180ea46cb4aa4f07ae17db4e8755a
Xevien <xevien96@gmail.com>
2017-05-26 20:27:46 +0200

    Levels können jetzt aus der txt automatisch in Serialisierte Level gespeichert werden

--- src/sokoban/LevelMaker.java
+++ src/sokoban/LevelMaker.java
@@ -1,21 +1,23 @@
 package sokoban;
 
 import javax.swing.*;
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LevelMaker {
 
     public static void main(String[] args) {
-        JFileChooser jfc = new JFileChooser(System.getProperty("user.dir")+"\\src\\sokoban\\Levels\\TextFileLevels");
+        JFileChooser jfc = new JFileChooser(System.getProperty("user.dir") + "\\src\\sokoban\\Levels\\TextFileLevels");
         int returnVal = jfc.showOpenDialog(null);
-        if(returnVal == JFileChooser.APPROVE_OPTION){
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             File levelFile = jfc.getSelectedFile();
             makeLevelsFromFile(levelFile);
         }
     }
 
-    public static void makeLevelsFromFile(File levelFile){
+    public static void makeLevelsFromFile(File levelFile) {
         try {
             FileReader input = new FileReader(levelFile);
             BufferedReader reader = new BufferedReader(input);
@@ -24,21 +26,20 @@ public class LevelMaker {
             String levelName;
             String level = "";
             String temp;
-            while((temp = reader.readLine()) != null){
-                if(temp.startsWith("Level")){
+            while ((temp = reader.readLine()) != null) {
+                if (temp.startsWith("Level")) {
                     levelNr = Integer.parseInt(temp.replaceAll("[\\D]", ""));
                     temp = reader.readLine();
                     levelName = temp;
-                    while(!(temp = reader.readLine()).equals("")){
-                        level += temp+"\n";
+                    while (!(temp = reader.readLine()).equals("")) {
+                        level += temp + "n";
                     }
                     new Level(worldName, levelNr, levelName, level);
+                    level = "";
                 }
             }
-        } catch (FileNotFoundException e){
-            System.out.println("File not found");
-        } catch (IOException e){
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println(e);
         }
     }
 }
