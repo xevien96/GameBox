0206c98482e9cf153f6143d995c8e4fdd3854fb3
BrandtMO <Marc_O_Brandt@gmx.de>
2017-06-07 14:09:51 +0200

    Levelnamen jetzt ohne ' ' am Anfang und Ende
    (cherry picked from commit ca5af79f9cf1a0a12f94ca9407c25e68a53a97e4)
    
    # Conflicts:
    #	src/sokoban/LevelMaker.java

---
+++ src/sokoban/LevelMaker.java
@@ -0,0 +1,49 @@
+package sokoban;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LevelMaker {
+
+    public static void main(String[] args) {
+        JFileChooser jfc = new JFileChooser(System.getProperty("user.dir") + "\\src\\sokoban\\Levels\\TextFileLevels");
+        int returnVal = jfc.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File levelFile = jfc.getSelectedFile();
+            makeLevelsFromFile(levelFile);
+        }
+    }
+
+    public static void makeLevelsFromFile(File levelFile) {
+        try {
+            FileReader input = new FileReader(levelFile);
+            BufferedReader reader = new BufferedReader(input);
+            String worldName = levelFile.getName().replace(".txt", "");
+            int levelNr;
+            String levelName = "";
+            String level = "";
+            String temp;
+            while ((temp = reader.readLine()) != null) {
+                if (temp.startsWith("Level")) {
+                    levelNr = Integer.parseInt(temp.replaceAll("[\\D]", ""));
+                    temp = reader.readLine();
+                    if (temp.startsWith("'")) {
+                        levelName = temp;
+                        temp = reader.readLine();
+                    }
+                    while (!temp.equals("")) {
+                        level += temp + "n";
+                        temp = reader.readLine();
+                    }
+                    new Level(worldName, levelNr, levelName, level);
+                    level = "";
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
