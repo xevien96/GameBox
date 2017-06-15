8d6accff9cc6f59f30555b14647d25bd131489bd
Xevien96 <xevien96@gmail.com>
2017-06-15 14:08:48 +0200

    moveUp() so zu nem drittel

--- src/sokoban/Level.java
+++ src/sokoban/Level.java
@@ -8,55 +8,92 @@ public class Level implements Serializable {
     private int levelNr;
     private String levelName;
     private ArrayList<ArrayList<Character>> level = new ArrayList<>();
+    private int posZeile;
+    private int posSpalte;
 
     public Level(String worldName, int levelNr, String levelName, String levelString) {
-        File directory = new File(System.getProperty("user.dir") + "\\src\\sokoban\\Levels\\" + worldName);
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
         this.worldName = worldName;
         this.levelNr = levelNr;
         this.levelName = levelName;
+        int posZ = 0;
+        int posS = 0;
         ArrayList<Character> zeile = new ArrayList<>();
         for (int i = 0; i < levelString.length(); i++) {
             switch (levelString.charAt(i)) {
                 case ' ':
                     zeile.add(' ');
+                    posS++;
                     break;
                 case '#':
                     zeile.add('#');
+                    posS++;
                     break;
                 case '@':
                     zeile.add('@');
+                    posZeile = posZ;
+                    posSpalte = posS;
                     break;
                 case '$':
                     zeile.add('$');
+                    posS++;
                     break;
                 case '.':
                     zeile.add('.');
+                    posS++;
                     break;
                 case '*':
                     zeile.add('*');
+                    posS++;
                     break;
                 case '+':
                     zeile.add('+');
+                    posZeile = posZ;
+                    posSpalte = posS;
                     break;
                 case 'n':
                     level.add(zeile);
                     zeile = new ArrayList<>();
+                    posZ++;
+                    posS = 0;
                     break;
                 default:
                     break;
             }
         }
-        try {
-            FileOutputStream fs = new FileOutputStream(directory + "\\" + String.valueOf(levelNr) + ".ser");
-            ObjectOutputStream os = new ObjectOutputStream(fs);
-            os.writeObject(this);
-            os.close();
-            fs.close();
-        } catch (IOException e) {
-            System.err.println(e);
+    }
+
+    public Character getLevel(int zeile, int spalte){
+        return level.get(zeile).get(spalte);
+    }
+
+    public void setLevel(int zeile, int spalte, Character c){
+        level.get(zeile).set(spalte, c);
+    }
+
+    public void moveUp(){
+        if(getLevel(posZeile-1, posSpalte).equals(' ')){
+            if(getLevel(posZeile, posSpalte).equals('@')){
+                setLevel(posZeile, posSpalte, ' ');
+                posZeile--;
+                setLevel(posZeile, posSpalte, '@');
+            }
+            else if(getLevel(posZeile, posSpalte).equals('+')){
+                setLevel(posZeile, posSpalte, '.');
+                posZeile--;
+                setLevel(posZeile, posSpalte, '@');
+            }
         }
+    }
+
+    public void moveDown(){
+
+    }
+
+    public void moveLeft(){
+
+    }
+
+    public void moveRight(){
+
     }
 }
